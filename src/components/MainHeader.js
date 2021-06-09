171168f9a0bca5908dc28766a2ffa6d8eb5aaa1b
Apeksha Singh <apekshasingh664@gmail.com>
2021-06-09 22:37:26 +0530

    first commit

---
+++ src/components/MainHeader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './MainHeader.module.css';
+
+const MainHeader = () => {
+  return (
+  <header className='header'>
+    <nav>
+      <ul>
+        <li>
+          <NavLink activeClassName='active' to='/welcome'>Welcome</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName='active' to='/products'>Products</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  );
+};
+export default MainHeader;
---
+++ src/pages/Products.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <section>
+      <h1>Products page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An online course</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Products;
